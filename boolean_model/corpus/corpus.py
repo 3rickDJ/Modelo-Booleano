8159e5bb38d2a016b012019a473a46cd71d95e5a
3rickDJ <erickdejesusmolina@gmail.com>
2023-10-01 11:16:20 -0600

    profile: improve the process of text from 130s -> 30s

--- boolean_model/corpus/corpus.py
+++ boolean_model/corpus/corpus.py
@@ -3,6 +3,8 @@ from .data_preprocessing import process_text
 import os
 from itertools import chain
 
+from concurrent.futures import ThreadPoolExecutor
+
 def read_corpus(path):
     # Read the corpus from the given path.
     files, corpus, stems = read_data(path)
@@ -15,10 +17,15 @@ def read_corpus(path):
 def read_data(path):
     # Read the data from the given path.
     files = read_file_paths(path)
-    corpus = [ process_text.ProcessData(file) for file in files ]
+    corpus = concurrent_read(files)
     stems = set( chain.from_iterable( [ process_data.get_stems() for process_data in corpus ] ) )
     # Return the corpus.
     return files, corpus, stems
+
+def concurrent_read(files):
+    with ThreadPoolExecutor(max_workers=len(files)) as executor:
+        corpus = executor.map(lambda file: process_text.ProcessData(file), files)
+        return list(corpus)
 
 def read_file_paths(path):
     # Read the file paths from the given path.
